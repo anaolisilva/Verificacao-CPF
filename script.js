12866b75ac9213fa714c9406de7313094efcd241
Ana Oliveira <vianello.ana@gmail.com>
2021-08-14 18:09:55 -0300

    Versão do código utilizando funções nativas JavaScript

--- script.js
+++ script.js
@@ -1,76 +1,63 @@
 //Escrito por Ana Oliveira Silva - 14/08/21
+//Explorando melhor funções nativas do JavaScript.
 
 function validacao() {
+
     let cpf = document.getElementById('cpf').value
 
-    let resultValidacao
-    let dv1
-    let dv2
-    let verificadorDv1
-    let verificadorDv2
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('fail').style.display = 'none';
 
-    if (cpf.length != 11){
-        resultValidacao = false
-    } else {
-
-        let cpfCopia = cpf
+    resultValidacao = validaCPF(cpf)
     
-        resultValidacao
-    
-        dv2 = cpf % 10
-        dv1 = parseInt((cpf % 100)/10)
-    
-        //com substring:
-        // dvs = cpf.substring(0,9)
-    
-        let intermediario
-        let soma = 0
-        let j = 1 //j é o número do dígito
-    
-        for (let i = 10; i >= 2; i--) {
-            intermediario = parseInt(cpfCopia / Math.pow(10, i))
-            cpfCopia = cpfCopia - (intermediario * Math.pow(10, i))
-            intermediario = intermediario * j
-            soma = soma + intermediario
-            j++
-        }
-    
-        verificadorDv1 = soma % 11
-    
-        if (verificadorDv1 == 10){
-            verificadorDv1 = 0
-        }
-    
-        soma = 0
-        j = 0
-        cpfCopia = cpf
-    
-        for (let i = 10; i >= 2; i--) {
-            intermediario = parseInt(cpfCopia / Math.pow(10, i))
-            cpfCopia = cpfCopia - (intermediario * Math.pow(10, i))
-            intermediario = intermediario * j
-            soma = soma + intermediario
-            j++
-        }
-    
-        soma = soma + (verificadorDv1 * 9)
-        verificadorDv2 = soma % 11
-    
-        if (verificadorDv2 == 10){
-            verificadorDv2 = 0
-        }
-    }
-
-
-    if (verificadorDv1 == dv1 && verificadorDv2 == dv2) {
-        resultValidacao = true
-    } else {
-        resultValidacao = false
-    }
-
     if (resultValidacao) {
         document.getElementById('success').style.display = 'block';
     } else {
         document.getElementById('fail').style.display = 'block';
     }
+}
+
+function validaCPF(cpf) {
+
+    if (cpf.length != 11){
+        return false
+    }
+
+    let dv1
+    let dv2
+    let verificadorDv1
+    let verificadorDv2
+    let cpfNumeros = cpf.substring(0,9) //Pega incluindo a posição 0 e excluindo a 9 (vai até a 8)
+    console.log(cpfNumeros)
+    
+    dv2 = cpf.charAt(10)
+    dv1 = cpf.charAt(9)
+    console.log(dv1)
+    console.log(dv2)
+
+    let soma = 0
+
+    for (let i = 10; i >= 2; i--) {
+        soma += cpfNumeros.charAt(10 - i) * (11 - i)
+    }
+    
+    //operador ternário
+    verificadorDv1 = (soma % 11) == 10 ? 0 : soma % 11
+    
+
+    soma = 0
+
+    for (let i = 10; i >= 2; i--) {
+        soma += cpfNumeros.charAt(10 - i) * (10 - i)
+    }
+
+    soma = soma + (verificadorDv1 * 9)
+    verificadorDv2 = (soma % 11) == 10 ? 0 : soma % 11
+
+    if (verificadorDv1 == dv1 && verificadorDv2 == dv2) {
+        return true
+    } else {
+        return false
+    }
+    
 }
